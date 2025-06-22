d92c4c15d325fbb4020475e892e620078f683873
Giovanni Salinas <gsalinaslopez.cs01@nctu.edu.tw>
2025-06-23 07:39:32 +0800

    feat: LR channels, CLI selection using --channel left/right

--- client/client_settings.hpp
+++ client/client_settings.hpp
@@ -65,6 +65,10 @@ struct ClientSettings
         SampleFormat sample_format;
         SharingMode sharing_mode{SharingMode::unspecified};
         Mixer mixer;
+		/// Output channel mode: "left", "right", or "stereo" CAPULLO
+		std::string channel{"stereo"};
+		/// Number of output channels: 1 for mono, 2 for stereo (derived from `channel`) CAPULLO
+		int channel_count{2};
     };
 
     struct Logging
